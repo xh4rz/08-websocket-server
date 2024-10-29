89138214255b699afe1ccb8e5164af83d350dcd4
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-10-29 13:24:06 -0500

    221. Broadcast - Ordenar nuestro código

--- models/server.js
+++ models/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { socketController } = require('../sockets/controller');
 
 class Server {
 	constructor() {
@@ -33,17 +34,7 @@ class Server {
 	}
 
 	sockets() {
-		this.io.on('connection', (socket) => {
-			socket.on('disconnect', () => {
-				// console.log('Cliente desconectado', socket.id);
-			});
-
-			socket.on('enviar-mensaje', (payload, callback) => {
-				const id = 123456789;
-				callback(id);
-				// this.io.emit('enviar-mensaje', payload);
-			});
-		});
+		this.io.on('connection', socketController);
 	}
 
 	listen() {
