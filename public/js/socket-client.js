3b46eaab5fecadc44616e8efe09cf9d6591bb642
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-10-29 12:09:52 -0500

    217. Mensajes de conexión y desconexión - Cliente

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,1 +1,19 @@
+// referencias
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+
 const socket = io();
+
+socket.on('connect', () => {
+	console.log('Conectado');
+
+	lblOffline.style.display = 'none';
+	lblOnline.style.display = '';
+});
+
+socket.on('disconnect', () => {
+	console.log('Desconectado');
+
+	lblOnline.style.display = 'none';
+	lblOffline.style.display = '';
+});
