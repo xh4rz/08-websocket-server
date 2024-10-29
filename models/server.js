60a06dbcbb72693f6be65af3795a6af16e06fe38
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2024-10-29 13:10:50 -0500

    219. Emitir desde el servidor - Escuchar en el cliente

--- models/server.js
+++ models/server.js
@@ -34,14 +34,12 @@ class Server {
 
 	sockets() {
 		this.io.on('connection', (socket) => {
-			console.log('cliente conectado', socket.id);
-
 			socket.on('disconnect', () => {
 				console.log('Cliente desconectado', socket.id);
 			});
 
 			socket.on('enviar-mensaje', (payload) => {
-				console.log(payload);
+				this.io.emit('enviar-mensaje', payload);
 			});
 		});
 	}
--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -8,17 +8,21 @@ const btnEnviar = document.querySelector('#btnEnviar');
 const socket = io();
 
 socket.on('connect', () => {
-	console.log('Conectado');
+	// console.log('Conectado');
 
 	lblOffline.style.display = 'none';
 	lblOnline.style.display = '';
 });
 
 socket.on('disconnect', () => {
-	console.log('Desconectado');
+	// console.log('Desconectado');
 
 	lblOnline.style.display = 'none';
 	lblOffline.style.display = '';
+});
+
+socket.on('enviar-mensaje', (payload) => {
+	console.log(payload);
 });
 
 btnEnviar.addEventListener('click', () => {
